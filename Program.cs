0fcfe06871c7f7dcd2f3919ecdddc2170025ed85
Franksmallz <49909346+Franksmallz@users.noreply.github.com>
2021-04-24 08:40:53 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -9,22 +10,22 @@ using System.Threading.Tasks;
 
 namespace DispatchMgt
 {
-    public class Program
-    {
-        public static void Main(string[] args)
+    
+
+        public class Program
         {
-            CreateHostBuilder(args).Build().Run();
+            public static void Main(string[] args)
+            {
+                var host = CreateWebHostBuilder(args).
+                    UseKestrel().
+                    UseUrls("http://0.0.0.0:" + Environment.GetEnvironmentVariable("PORT")).
+                    Build();
+
+                   host.Run();
+            }
+
+            public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+                    WebHost.CreateDefaultBuilder(args)
+                    .UseStartup<Startup>();
         }
-
-        public static IHostBuilder CreateHostBuilder(string[] args)
-            
-
-            =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                    webBuilder.UseUrls("http://*:" + Environment.GetEnvironmentVariable("PORT"));
-                });
     }
-}
